44bb0919aa6ac71b42209f105b4934de6ebd7d3b
OscarA <Oscar Ames>
2015-07-06 20:32:13 -0400

    save

--- jurassic.py
+++ jurassic.py
@@ -1,36 +1,71 @@
-"""PINK FLUFFY UNICORNS"""
+"""Welcome, to Jurassic World"""
 
 import colors as c
 from utils import ask
 
 intro = c.red + '''
-Welcome, to the pink fluffy unicorns quiz
+Welcome, to the Jurassic Park quiz
 '''
 
 def q1():
-    answer = ask(c.red + 'Question 1: What colour are the unicorns?')
-    if answer == "pink":
-        print('And what a lovely color it is')
+    answer = ask(c.red + 'Question 1: What was the base gene of the Indominous Rex?')
+    if answer == "tyranosaurus rex":
+        print('good choice')
         return True
     print('That is incorrect')
     return False
 
 def q2():
-    answer = ask(c.red + 'Question 2: Where are they dancing?')
-    if answer == "rainbows":
-        print('Its a wonderful place to dance on, try it')
+    answer = ask(c.red + 'Question 2: How did Owen escape the Indominous?')
+    if answer == "he covered himself in oil":
+        print('The indominous could not smell him when he did that')
         return True
     print('That is incorrect')
     return False
 
 def q3():
-    answer = ask(c.red + 'Question 3: Please use one word to describe the texture of their magical fur?')
-    if answer == "smiles":
-        print('It feels wonderful on your skin' + c.clear)
+    answer = ask(c.red + 'Question 3: How did the Indominous hid from thermal tech?')
+    if answer == "tree frog DNA alowed it to":
+        print('correct')
         return True
-    print('That is incorrect' + c.clear)
+    print('That is incorrect')
     return False
 
-questions = [q1,q2,q3]
+def q4():
+    answer = ask(c.red + 'Question 3: How did the Indominous die?')
+    if answer == "eaten by a mosasaurus":
+        print('correct')
+        return True
+    print('That is incorrect')
+    return False
+
+def q5():
+    answer = ask(c.red + 'Question 3: how many of the 4 raptors died?')
+    if answer == "3":
+        print('correct, its sad that any of them died')
+        return True
+    print('That is incorrect')
+    return False
+
+def q6():
+    answer = ask(c.red + 'how did the chief of security die?')
+    if answer == "he was eaten by raptors":
+        print('correct')
+        return True
+    print('That is incorrect')
+    return False
+
+def q7():
+    answer = ask(c.red + 'how did the owner of the company die??')
+    if answer == "he crashed":
+        print('correct')
+        return True
+    print('That is incorrect')
+    return False
+
+
+
+
+questions = [q1,q2,q3,q4,q5,q6,q7]
 
 
